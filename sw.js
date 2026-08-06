ce7cd7d5e87ec2526629498c8b3b2a0f85d0e87b
evcscavalcante <43319739+evcscavalcante@users.noreply.github.com>
2026-08-06 16:27:35 -0300

    feat: load indicator dashboard in PWA

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = "bafometro-nfc-v6";
+const CACHE_NAME = "bafometro-nfc-v7";
 const APP_SHELL = [
   "./",
   "./manifest.webmanifest",
   "./theme-google.css",
+  "./indicadores.css",
+  "./indicadores.js",
   "./favicon-v1.png",
   "./icon-192-v1.png",
   "./icon-512-v1.png"
@@ -19,6 +21,12 @@ async function prepareHtml(response) {
   const additions = [];
   if (!html.includes("theme-google.css")) {
     additions.push('<link rel="stylesheet" href="./theme-google.css">');
+  }
+  if (!html.includes("indicadores.css")) {
+    additions.push('<link rel="stylesheet" href="./indicadores.css">');
+  }
+  if (!html.includes("indicadores.js")) {
+    additions.push('<script src="./indicadores.js" defer></script>');
   }
   if (!html.includes("favicon-v1.png")) {
     additions.push('<link rel="icon" type="image/png" sizes="32x32" href="./favicon-v1.png">');
